bcc89f5308752821ade059ee4eaa317c351f381e
Meiriele <meiriele_dias@yahoo.com.br>
2021-05-30 14:23:03 -0300

    Cria um projeto spring boot vazio

---
+++ Quest/src/main/java/br/ufjf/dcc193/meiriele/Quest/QuestApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.meiriele.Quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestApplication.class, args);
+	}
+
+}
